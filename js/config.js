f48dc1df2d10179e8eca3ef9c255e0959a25e28d
EC2 Default User <dylan.martyn@cloudreach.com>
2019-05-14 17:46:59 +0000

    Add Cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_LKdWsJnD1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2bnrt7ta7tat8n7lt70dd46via', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
